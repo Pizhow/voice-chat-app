65a0dc367ab4cef3d8ace7592391dc251d3f0db0
Serji <your-email@example.com>
2025-06-20 20:13:21 +0300

    чат

--- server.js
+++ server.js
@@ -1,44 +1,35 @@
 
 const express = require('express');
 const http = require('http');
-const socketIO = require('socket.io');
+const { Server } = require('socket.io');
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server);
-const users = {};
+const io = new Server(server);
+
+const users = new Map();
 
 app.use(express.static('public'));
 
-io.on('connection', socket => {
-  let userId = null;
+io.on('connection', (socket) => {
+    console.log('Пользователь подключён');
 
-  socket.on('join', (id) => {
-    userId = id;
-    users[socket.id] = id;
-    io.emit('users', Object.values(users));
-    socket.broadcast.emit('user-connected', id);
-  });
+    socket.on('join', (username) => {
+        users.set(socket.id, username);
+        io.emit('user-list', Array.from(users.values()));
+    });
 
-  socket.on('signal', (data) => {
-    for (const [sid, uid] of Object.entries(users)) {
-      if (uid === data.to) {
-        io.to(sid).emit('signal', { from: data.from, signal: data.signal });
-        break;
-      }
-    }
-  });
+    socket.on('message', (msg) => {
+        const sender = users.get(socket.id);
+        io.emit('message', { user: sender, text: msg, time: new Date().toLocaleTimeString() });
+    });
 
-  socket.on('chat-message', ({ from, text }) => {
-    io.emit('chat-message', { from, text });
-  });
-
-  socket.on('disconnect', () => {
-    if (userId) {
-      socket.broadcast.emit('user-disconnected', userId);
-      delete users[socket.id];
-      io.emit('users', Object.values(users));
-    }
-  });
+    socket.on('disconnect', () => {
+        users.delete(socket.id);
+        io.emit('user-list', Array.from(users.values()));
+    });
 });
 
-server.listen(3000, () => console.log('Server on port 3000'));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Сервер запущен на порту ${PORT}`);
+});
